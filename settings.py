59efdc2e6fa5ce0a99a09987cf0c3305a0e4ade6
sharanyaadiga <120501712+sharanyaadiga@users.noreply.github.com>
2024-07-13 22:12:40 +0200

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,11 @@
+#this is my settings file
+
+
+temp_topic ="HTWIoT/temp/s0585849"
+humi_topic ="HTWIoT/humi/s0585849"
+
+#mqtt_server
+brokerURL = "test.mosquitto.org"
+
+topic_sub = "HTWIoT/notification"
+message_interval = 20
